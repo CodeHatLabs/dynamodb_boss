3219818d9bf3a34066041f75f549c1ccc81b866b
Jeff Jarvis <jjarvis@codehatlabs.com>
2018-04-16 13:36:53 -0700

    refactor so the Pool receives and stores aws settings in its constructor (that default to conf values) and the Boss has aws settings passed in to the constructor instead of hard-wired pulling from conf

--- dynamodb_boss/boss.py
+++ dynamodb_boss/boss.py
@@ -7,12 +7,12 @@ from . import conf
 
 class DynamoDBBoss(object):
 
-    def __init__(self, table_name_prefix=''):
+    def __init__(self, aws_access_key_id, aws_secret_access_key, region_name,
+                                                        table_name_prefix=''):
         self.session = boto3.Session(
-            aws_access_key_id = conf.settings.DYNAMODB_BOSS_AWS_ACCESS_KEY_ID,
-            aws_secret_access_key = \
-                            conf.settings.DYNAMODB_BOSS_AWS_SECRET_ACCESS_KEY,
-            region_name = conf.settings.DYNAMODB_BOSS_REGION
+            aws_access_key_id = aws_access_key_id,
+            aws_secret_access_key = aws_secret_access_key,
+            region_name = region_name
             )
         self.dynamodb = self.session.resource('dynamodb')
         self.table_name_prefix = table_name_prefix
@@ -30,8 +30,23 @@ class DynamoDBBoss(object):
 
 class DynamoDBBossPool(ResourcePool):
 
+    def __init__(self, aws_access_key_id=None, aws_secret_access_key=None,
+                                    region_name=None, table_name_prefix=''):
+        self.aws_access_key_id = aws_access_key_id \
+                        or conf.settings.DYNAMODB_BOSS_AWS_ACCESS_KEY_ID
+        self.aws_secret_access_key = aws_secret_access_key \
+                        or conf.settings.DYNAMODB_BOSS_AWS_SECRET_ACCESS_KEY
+        self.region_name = region_name or conf.settings.DYNAMODB_BOSS_REGION
+        self.table_name_prefix = table_name_prefix \
+                        or conf.settings.DYNAMODB_BOSS_TABLE_NAME_PREFIX
+
     def _CreateNewResource(self):
-        return DynamoDBBoss(conf.settings.DYNAMODB_BOSS_TABLE_NAME_PREFIX)
+        return DynamoDBBoss(
+            self.aws_access_key_id,
+            self.aws_secret_access_key,
+            self.region_name,
+            self.table_name_prefix
+            )
 
 
 dynamodb_boss_pool = DynamoDBBossPool()
